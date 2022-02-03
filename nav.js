0a8ac2daf053188588219d276ff443047ae6770f
Masroor Khonkhodzhaev <masroor47@icloud.com>
2022-02-02 19:56:50 -0500

    menu button works

--- nav.js
+++ nav.js
@@ -2,5 +2,11 @@ const navContainer = document.querySelector(".nav-container");
 const navToggle = document.querySelector(".mobile-nav-toggle");
 
 navToggle.addEventListener("click", () => {
+    const visibility = navContainer.getAttribute("data-visible");
 
+    if (visibility === "false") {
+        navContainer.setAttribute("data-visible", "true");
+    } else {
+        navContainer.setAttribute("data-visible", "false");
+    }
 })
